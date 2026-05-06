c7922377c96c0a6026ff62fabae7e9e4b6162f80
TobyLock68 <tobylock68@gmail.com>
2026-05-06 14:06:20 +0100

    coded section and vosk move

---
+++ gemini_vosk.py
@@ -0,0 +1,106 @@
+import os
+import csv
+from google import genai
+import queue
+import json
+import sounddevice as sd
+from datetime import datetime
+from vosk import Model, KaldiRecognizer
+import time
+import wave
+
+#Configuring Models
+GEMINI_MODEL = "gemini-2.5-flash"
+VOSK_MODEL = "vosk-model-en-us-0.22-lgraph"
+SAMPLE_RATE = 16000
+OUTPUT_CSV = "group_transcript.csv"
+
+model = Model(VOSK_MODEL)
+
+#gemini transcript clean
+def corrected_text(raw_text):
+    client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+    prompt = f"Correct this transcript and return only the corrected sentence: {raw_text}"
+    response = client.models.generate_content(model = GEMINI_MODEL, contents = prompt)
+    return response.text.strip()
+
+def realtime_transcription():
+    q = queue.Queue()
+    def callback(indata,frames,time,status): #start recording and transcribing
+        if status:
+            print(status)
+        q.put(bytes(indata))
+    
+    recognizer = KaldiRecognizer(model,SAMPLE_RATE)
+    start_time =time.time()
+   
+    print("Recording ... Press Ctrl+C to finish.")
+
+    try:
+        with sd.RawInputStream(
+        samplerate=SAMPLE_RATE,
+        blocksize=8000,
+        dtype="int16",
+        channels=1,
+        callback=callback
+    ):
+            while True:
+                data = q.get()
+                recognizer.AcceptWaveform(data)
+    
+    except KeyboardInterrupt:
+        print("\n Recording stopped")
+    
+    final_result = json.loads(recognizer.FinalResult())
+    duration = round(time.time()-start_time, 2)
+    return final_result.get("text",""), duration
+
+def save_to_csv(data):
+    file_exists = os.path.exists(OUTPUT_CSV)
+    with open(OUTPUT_CSV, "a", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f,fieldnames=["timestamp","name","raw_text_vosk","text","time_taken_sec"])
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow(data)
+
+def transcribe_file(file_path):
+    wf = wave.open(file_path, "rb")
+    recognizer = KaldiRecognizer(model, wf.getframerate())
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        recognizer.AcceptWaveform(data)
+    
+    final_result = json.loads(recognizer.FinalResult())
+    duration = round(wf.getnframes() / wf.getframerate(), 2)
+    return final_result.get("text", ""), duration
+
+
+def main():
+    print("Meeting Pipeline")
+
+    while True:
+        mode = input("Choose mode: [F]ile, [R]ealtime or [Q]uit:").lower()
+        if mode == 'q':
+            break
+        if mode == 'r':
+            raw_text, duration = realtime_transcription()
+        elif mode == 'f':
+            path = input("Enter file path to .wav file:")
+            raw_text,duration = transcribe_file(path)
+        else:
+            continue
+
+        correct_text = corrected_text(raw_text)
+
+        save_to_csv({
+            "timestamp": datetime.now().isoformat(),
+            "raw_text_vosk": raw_text,
+            "text": correct_text,
+            "time_taken_sec": duration
+            })
+        print("Transcription complete")
+
+if __name__ == "__main__":
+    main()
--- gemini_vosk_code.py
+++ gemini_vosk_code.py
@@ -7,4 +7,66 @@ import sounddevice as sd
 from datetime import datetime
 from vosk import Model, KaldiRecognizer
 import time
+import wave
 
+#Configuring Models
+GEMINI_MODEL = "gemini-2.5-flash"
+VOSK_MODEL = "vosk-model-en-us-0.22-lgraph"
+SAMPLE_RATE = 16000
+OUTPUT_CSV = "group_transcript.csv"
+
+#gemini transcript clean
+def corrected_text(raw_text):
+    client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+    prompt = f"Correct this transcript and return only the corrected sentence: {raw_text}"
+    response = client.models.generate_content(model = GEMINI_MODEL, contents = prompt)
+    return response.text.strip()
+
+
+#def record_turn(): #maybe needed
+q = queue.Queue()
+
+def callback(indata,frames,time,status): #start recording and transcribing
+    if status:
+        print(status)
+    q.put(bytes(indata))
+    
+    model = Model(VOSK_MODEL)
+    recognizer = KaldiRecognizer(model,SAMPLE_RATE)
+    start_time =time.time()
+   
+    print("Recording ... Press ENTER to finish.")
+
+    full_text = ""
+
+    with sd.RawInputStream(
+        samplerate=SAMPLE_RATE,
+        blocksize=8000,
+        dtype="int16",
+        channels=1,
+        callback=callback
+    ):
+        while True:
+            data = q.get()
+
+            if recognizer.AcceptWaveform(data):
+                result = json.loads(recognizer.Result())
+                text = result.get("text","")
+                if text:
+                    print("You said:", text)
+                    full_text += text + " "
+    
+    final_result = json.loads(recognizer.FinalResult())
+    final_text += final_result.get("text","")
+
+    duration = round(time.time()-start_time, 1)
+    return full_text.strip(), duration
+    
+
+
+#def transcribe_file(): #may be needed as i think vosk is more an real time audio input
+
+
+
+#def main(): 
+    #for the transcription we may need an option for both microphone and file input
