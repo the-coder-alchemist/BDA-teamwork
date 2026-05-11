3d4e2f331f39dbcc731a6ac9fca7acaaf3d1c582
Carys Williams <caryswilliams@Caryss-MacBook-Air.local>
2026-05-11 10:20:31 +0100

    added num_words_in_text column

--- feature_enrichement.py
+++ feature_enrichement.py
@@ -5,10 +5,14 @@ with open("group_transcript.csv", 'r', encoding='utf-8') as infile, \
      open("group_transcript_enriched.csv", 'w', encoding='utf-8', newline='') as outfile:
     
     reader = csv.DictReader(infile)
-    writer = csv.DictWriter(outfile, fieldnames=reader.fieldnames + ['has_question_mark'])
+    writer = csv.DictWriter(outfile, fieldnames=reader.fieldnames + ['has_question_mark', 'num_words_in_text',])
+
     
     writer.writeheader()
     for row in reader:
         row['has_question_mark'] = '?' in row.get('text', '')
+        row['num_words_in_text'] = len(row.get('text', '').split())
         writer.writerow(row)
+
+    
 
